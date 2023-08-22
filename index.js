2bdb177f3b65a38530f3a42414b094d2ce3c735e
shawnconway <shawnway210@gmail.com>
2023-08-21 21:38:17 -0400

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <= 400){
+    result = 'This one is on me!'
+  } 
+  else if(ride < 2000){
+    result = 'That will be twenty bucks.'
+  }
+  else {(ride > 2000) ;
+    result = 'I will gladly take your thirty bucks.'
+  }
+  if (ride > 2500) {
+    result = 'No can do.' 
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   return city === "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
